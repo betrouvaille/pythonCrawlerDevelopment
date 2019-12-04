cb97e89ef1c6eb41cb7e26dfa311abb0f21a2bf4
lfl7824833 <lfl7824833@vip.qq.com>
2019-12-04 18:24:52 +0800

    1

--- chapter6/mongodb_try.py
+++ chapter6/mongodb_try.py
@@ -12,5 +12,29 @@ collection = database[col_name]
 # collection.insert_one(data)
 
 
-print([x for x in collection.find()])
-print([x for x in collection.find({'age': 19})])
+print('全部返回', [x for x in collection.find()], '\n')
+print('查询哪些记录age为19', [x for x in collection.find({'age': 19})], '\n')
+print('只返回某些字段', [x for x in collection.find(
+    {}, {'name': 1, 'salary': 1, '_id': 0})], '\n')
+
+
+print('查询年纪>=18', [x for x in collection.find(
+    {'age': {'$gte': 18}}, {'name': 1, 'salary': 1, 'age': 1, '_id': 0})], '\n')
+
+print('按工资排序，显示名字和工资', [x for x in collection.find(
+    {}, {'name': 1, 'salary': 1, "_id": 0}).sort('salary', -1)], '\n')
+
+
+# 怎么 【更新】 MongoDB！！！！！
+
+# collection.update_one({'age':20}, {'$set':{'name': 'kingname'}})  重置【第一个】年龄为20的人的名字为kingname
+# collection.update_many({'age':20}, {'$set':{'age': 30}})  重置【所有】年龄为20的人的名字为kingname
+
+
+# 怎么 【删除】 一些记录！！！！
+# collection.delete_one({'name': 'kingname'}) 删除【第一个】叫kingname的人的记录
+# collection.delete_many({'name': 'kingname'})  删除【所有】叫kingname的人的记录
+
+
+# 怎么 【去重】！！
+# collection.distinct('列名')
