bf728438eb27d812aa81f4ccc279453289c407cf
quzhang5218 <quzhang5218@163.com>
2019-12-17 17:47:36 +0800

    Update README.md

--- Animal_farm/text1.py
+++ Animal_farm/text1.py
@@ -1,40 +1,59 @@
-import time
-
-import requests
-# html = requests.get('https://www.baidu.com').content.decode()
-# print(html)
-
-from multiprocessing.dummy import Pool
-
-
+# import time
+#
+# import requests
+# # html = requests.get('https://www.baidu.com').content.decode()
+# # print(html)
+#
+# from multiprocessing.dummy import Pool
 #
 #
-# def calc_power(num):
-#     return num * num
+# #
+# #
+# # def calc_power(num):
+# #     return num * num
+# #
+# #
+# # pool = Pool(3)
+# # origin_num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# # result = pool.map(calc_power, origin_num)
+# # print(result)
 #
 #
-# pool = Pool(3)
-# origin_num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-# result = pool.map(calc_power, origin_num)
-# print(result)
-
-
-def query(url):
-    requests.get(url)
-
-
+# def query(url):
+#     requests.get(url)
+#
+#
+# # start = time.time()
+# # for i in range(100):
+# #     query('https://mi.com')
+# #
+# # end = time.time()
+# # print('单线程时间', {end - start})
 # start = time.time()
+# url_list = []
 # for i in range(100):
-#     query('https://mi.com')
+#     url_list.append('https://baidu.com')
 #
+# pool = Pool(5)
+# pool.map(query, url_list)
 # end = time.time()
-# print('单线程时间', {end - start})
-start = time.time()
-url_list = []
-for i in range(100):
-    url_list.append('https://baidu.com')
+# print('多线程时间', {end - start})
 
-pool = Pool(5)
-pool.map(query, url_list)
-end = time.time()
-print('多线程时间', {end - start})
+
+def yanghuisanjia():
+    """
+    杨辉三角
+    :return:
+    """
+    l=[]
+    for n in range(30):
+        r = [1]
+        l.append(r)
+        if n == 0:
+            print(r)
+            continue
+        for m in range(1, n):
+            r.append(l[n-1][m-1]+l[n-1][m])
+        r.append(1)
+        print(r)
+
