6d5881fccde41bb7099c86fbe96a54e9471d238e
quzhang5218 <quzhang5218@163.com>
2019-12-18 16:14:47 +0800

    Update README.md

--- ywk_practice.py
+++ ywk_practice.py
@@ -1,10 +1,8 @@
 import time
-
 import requests
+from multiprocessing.dummy import Pool
 # # html = requests.get('https://www.baidu.com').content.decode()
 # # print(html)
-
-from multiprocessing.dummy import Pool
 
 
 def query(url):
@@ -31,24 +29,22 @@ def ca_time():
     print('多线程时间', {end - start})
 
 
-# #
-# #
-# # def calc_power(num):
-# #     return num * num
-# #
-# #
-# # pool = Pool(3)
-# # origin_num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-# # result = pool.map(calc_power, origin_num)
-# # print(result)
-
-
-# # start = time.time()
-# # for i in range(100):
-# #     query('https://mi.com')
-# #
-# # end = time.time()
-# # print('单线程时间', {end - start})
+#
+#
+# def calc_power(num):
+#     return num * num
+#
+#
+# pool = Pool(3)
+# origin_num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# result = pool.map(calc_power, origin_num)
+# print(result)
+# start = time.time()
+# for i in range(100):
+#     query('https://mi.com')
+#
+# end = time.time()
+# print('单线程时间', {end - start})
 
 
 def yanghuisanjia():
@@ -74,7 +70,12 @@ def get_suning():
     苏宁价格json
     :return:
     """
-    url = 'https://ds.suning.com/ds/generalForTile/000000010597918588____R1901001_000060021,000000011346304380____R1901001_000060021,000000011177564390____R1901001_000060864,000000011116456488____R1901001_000060DER,000000011222349674____R1901001_000066138,000000011446042332____R1901001_000060864,000000010673154488____R1901001_000066138,000000011210598958____R1901001_000060DER,000000000690105194____R1901001_000060021,000000011346304317____R1901001_000060021-010-2-0000000000-1--ds0000000003792.jsonp?callback=ds0000000003792'
+    url = 'https://ds.suning.com/ds/generalForTile/000000010597918588____R1901001_000060021,' \
+          '000000011346304380____R1901001_000060021,000000011177564390____R1901001_000060864,' \
+          '000000011116456488____R1901001_000060DER,000000011222349674____R1901001_000066138,' \
+          '000000011446042332____R1901001_000060864,000000010673154488____R1901001_000066138,' \
+          '000000011210598958____R1901001_000060DER,000000000690105194____R1901001_000060021,' \
+          '000000011346304317____R1901001_000060021-010-2-0000000000-1--ds0000000003792.jsonp?callback=ds0000000003792'
     url_json = requests.get(url).content.decode()
     print(url_json)
 
